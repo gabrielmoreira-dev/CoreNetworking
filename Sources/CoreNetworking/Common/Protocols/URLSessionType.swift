ade927c1590d6f4273d84c5f159ad48ce0500230
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-05-18 17:15:55 -0300

    fix: Fixed iOS version

--- Sources/CoreNetworking/Common/Protocols/URLSessionType.swift
+++ Sources/CoreNetworking/Common/Protocols/URLSessionType.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public protocol URLSessionType {
-    @available(macOS 12.0, iOS 13.0, *)
+    @available(macOS 12.0, iOS 15.0, *)
     func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
 }
 
--- Tests/CoreNetworkingTests/Remote/RemoteRestClientTests.swift
+++ Tests/CoreNetworkingTests/Remote/RemoteRestClientTests.swift
@@ -96,7 +96,7 @@ final class URLSessionSpy: URLSessionType {
     private(set) var request: URLRequest?
     var data: Data?
 
-    @available(macOS 12.0, iOS 13.0, *)
+    @available(macOS 12.0, iOS 15.0, *)
     func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse) {
         self.request = request
         guard let data else {
